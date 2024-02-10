1230b0b7d2772cf81c6657962415c7585fced778
Arthur <arturo.kudabaev@mail.ru>
2024-02-10 22:41:47 +0500

    Changes 14 brands

--- src/js/spoler.js
+++ src/js/spoler.js
@@ -4,18 +4,22 @@ function initSpoler() {
     elementSpolers.forEach(spoler => {
         const btn = spoler.querySelector('.spoler__btn');
         const content = spoler.querySelector('.spoler__content');
+        let isHidden;
 
         function spolerHandler() {
-            content.classList.toggle('hide');
-
-            if (btn.classList.contains('hide')) {
-                btn.innerText = 'Скрыть'
-            } else {
+            if (isHidden) {
+                isHidden = false
+                content.classList.add('hide')
+                btn.classList.add('hide')
                 btn.innerText = 'Показать все'
+            } else {
+                isHidden = true
+                btn.innerText = 'Скрыть'
+                content.classList.remove('hide')
+                btn.classList.remove('hide')
             }
-
-            btn.classList.toggle('hide');
         }
+
 
         btn.addEventListener('click', spolerHandler);
     })
