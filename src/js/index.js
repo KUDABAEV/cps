dcd20c1a2205d0697421a798e019955ffab1dd2a
Arthur <arturo.kudabaev@mail.ru>
2024-02-23 23:28:51 +0500

    add page.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../scss/index.scss'
 import { initSliderBrands } from "./sliders.js";
 import { initSpoler } from "./spoler.js";
 import { debounce } from "./utils.js";
+import { addOverlay, initPage, removeOverlay } from './page.js';
 
 
 function init() {
@@ -11,3 +12,10 @@ function init() {
 
 init();
 window.addEventListener('resize', debounce(init));
+
+const btn1 = document.querySelector('#b1');
+const btn2 = document.querySelector('#b2');
+
+
+btn1.addEventListener('click', addOverlay)
+btn2.addEventListener('click', removeOverlay)
