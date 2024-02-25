d310a899b20ecd129962a537fce4f391b5970710
Arthur <arturo.kudabaev@mail.ru>
2024-02-25 16:08:12 +0500

    types button show

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 import '../scss/index.scss'
 import { initSliderBrands } from "./sliders.js";
+import { initSliderTypes } from './typesSlider.js';
 import { initSpoler } from "./spoler.js";
 import { debounce } from "./utils.js";
 import { initNavigation } from './navigation.js';
 
 function init() {
     initSliderBrands();
+    initSliderTypes();
     initSpoler();
     initNavigation();
 }
