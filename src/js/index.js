f742debd7e109a439d74160d9d377d919654a68b
Arthur <arturo.kudabaev@mail.ru>
2024-02-26 00:50:32 +0500

    services slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,18 @@
 import '../scss/index.scss'
-import { initSliderBrands } from "./sliders.js";
-import { initSliderTypes } from './typesSlider.js';
-import { initSpoler } from "./spoler.js";
-import { debounce } from "./utils.js";
-import { initNavigation } from './navigation.js';
+import { initSliderBrands } from './sliders.js'
+import { initSliderTypes } from './typesSlider.js'
+import { initSpoler } from './spoler.js'
+import { debounce } from './utils.js'
+import { initNavigation } from './navigation.js'
+import { initSliderServices } from './servicesSlider.js'
 
 function init() {
-    initSliderBrands();
-    initSliderTypes();
-    initSpoler();
-    initNavigation();
+  initSliderBrands()
+  initSliderTypes()
+  initSliderServices()
+  initSpoler()
+  initNavigation()
 }
 
-init();
-window.addEventListener('resize', debounce(init));
-
+init()
+window.addEventListener('resize', debounce(init))
