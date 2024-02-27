9ba976dfc360e444a3957fdceee829496cb9c363
Arthur <arturo.kudabaev@mail.ru>
2024-02-27 20:46:51 +0500

    feedback add fonts

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,59 +1,56 @@
-import { addOverlay, removeOverlay } from "./page";
-import { debounce } from "./utils";
+import { addOverlay, removeOverlay } from './page'
+import { debounce } from './utils'
 
-const navigation = document.querySelector('.navigation');
-const burgerAll = document.querySelectorAll('.burger');
+const navigation = document.querySelector('.navigation')
+const burgerAll = document.querySelectorAll('.burger')
 
-let isNavigationActive = false;
-
+let isNavigationActive = false
 
 export function openNavigation() {
-  isNavigationActive = true;
-  navigation.classList.add('navigation_open');
-  addOverlay();
+  isNavigationActive = true
+  navigation.classList.add('navigation_open')
+  addOverlay()
 }
 
 export function closeNavigation() {
-  isNavigationActive = false;
-  navigation.classList.remove('navigation_open');
-  removeOverlay();
+  isNavigationActive = false
+  navigation.classList.remove('navigation_open')
+  removeOverlay()
 }
 
-
 export function toggleNavigation() {
-  const images = document.querySelectorAll('.burger img');
+  const images = document.querySelectorAll('.burger img')
 
-  if(isNavigationActive) {
-    closeNavigation();
-    images.forEach(img=> {
-      img.src = './images/burger-open.svg'
+  if (isNavigationActive) {
+    closeNavigation()
+    images.forEach((img) => {
+      img.src = './img/burger-open.svg'
     })
-  }else {
-    openNavigation();
-    images.forEach(img=> {
-      img.src = './images/burger.svg'
+  } else {
+    openNavigation()
+    images.forEach((img) => {
+      img.src = './img/burger.svg'
     })
   }
 }
 
 function updateFloating() {
-  const width = window.innerWidth;
+  const width = window.innerWidth
 
-  if(width < 1120){
+  if (width < 1120) {
     navigation.classList.add('navigation_floating')
-  }else {
+  } else {
     navigation.classList.remove('navigation_floating')
-    closeNavigation();
+    closeNavigation()
   }
 }
 
 export function initNavigation() {
-  burgerAll.forEach(item => {
-    item.addEventListener('click', toggleNavigation);
+  burgerAll.forEach((item) => {
+    item.addEventListener('click', toggleNavigation)
   })
 
   updateFloating()
 
-  window.addEventListener('resize', debounce(updateFloating));
+  window.addEventListener('resize', debounce(updateFloating))
 }
-
